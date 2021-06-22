f53cca8ed53fc722f2b9aebaafa3230608f5aac4
Karl Madl <karl_madl@yahoo.com>
2021-06-22 14:40:36 -0400

    added comments and removed most situation-particular values/parameters

--- Arduino_Reading/arduino_reading.py
+++ Arduino_Reading/arduino_reading.py
@@ -8,25 +8,31 @@ import math
 
 def temperature_reader():
 
-    ARDUINO_BOARD = pyfirmata.Arduino('COM3')
+    # pointing to the port the arduino is connected to and setting the correct pin to read
+    ARDUINO_BOARD = pyfirmata.Arduino("PORT")
     PYFIRMATA_ITERATOR = pyfirmata.util.Iterator(ARDUINO_BOARD)
     PYFIRMATA_ITERATOR.start()
-    analog_input = ARDUINO_BOARD.get_pin('a:0:i')
+    analog_input = ARDUINO_BOARD.get_pin("ANALOG IN PIN")
 
-    R1 = 10000
-    C1, C2, C3 = 1.009249522e-03, 2.378405444e-04, 2.019202697e-07
+    # constants to solve steihart-hart equation for the particular thermistor being used
+    R1 = 10000                                                      # resistence of the thermistor
+    C1, C2, C3 = 1.009249522e-03, 2.378405444e-04, 2.019202697e-07  # manufacturer LUT constants / Steinhart-Hart constants for particular thermistor
     readings = []
     
+    # get the 20 second average current temperature
     while len(readings) < 10:
         voltage_reading = analog_input.read()
         
         if type(voltage_reading) == float:
             voltage_reading *= 1023
+            
             R2 = R1 * (1023.0 / voltage_reading - 1.0)
             logR2 = math.log(R2)
+            
             T_kelvin = (1.0 / (C1 + C2*logR2 + C3*logR2*logR2*logR2))
             T_celsius = T_kelvin - 273.15
             T_fahrenheit = (T_celsius * 9.0)/ 5.0 + 32.0
+            
             readings.append(T_fahrenheit)
         
         time.sleep(2)
