48102b4ece6ff45610899bc21cdc731427f579ff
Karl Madl <karl_madl@yahoo.com>
2021-06-24 13:44:18 -0400

    removed credential_getter function from mysql_part, replaced it by importing a dictionary (gitignored) with credentials located in a new file  'credentials_file'

--- MySQL/mysql_part.py
+++ MySQL/mysql_part.py
@@ -1,31 +1,19 @@
+from credentials_file.credentials_location import My_SQL_credentials as CREDS   # imports dictionary of MySQL credentials and info (database, table)
 from mysql.connector import connect, Error
-
-
-# looks to local file to grab credentials to connect to SQL server, file should be formatted with each piece of info on a newline, in order as seen below
-def database_credential_getter():
-    with open(r"C:/Users/kimba/VSCode Projects/Room Temperature Project/room_temperature_database_credentials.txt", 'r') as f:
-        database_credentials = [line.rstrip('\n') for line in f]
-        HOST = database_credentials[0]
-        USERNAME = database_credentials[1]
-        PASSWORD = database_credentials[2]
-        DATABASE = database_credentials[3]
-        TABLE = database_credentials[4]
-        return HOST, USERNAME, PASSWORD, DATABASE, TABLE
 
 
 # creates the query and connects to the mysql database before inserting data dictionary as data into the table  
 def insert_into_MySQL(data: dict):
     
-    HOST, USERNAME, PASSWORD, DATABASE, TABLE = database_credential_getter()
-    query = f"INSERT INTO {TABLE} ({', '.join(data)}) VALUES ({('%s, '*len(data)).rstrip(', ')})"
+    query = f"INSERT INTO {CREDS['table']} ({', '.join(data)}) VALUES ({('%s, '*len(data)).rstrip(', ')})"
     args = [*data.values()]
     
     try:
         with connect(
-            host=HOST,
-            user=USERNAME,
-            password=PASSWORD,
-            database=DATABASE
+            host= CREDS['host'],
+            user= CREDS['user'],
+            password= CREDS['password'],
+            database= CREDS['database']
         ) as connection:
 
             cursor = connection.cursor()
