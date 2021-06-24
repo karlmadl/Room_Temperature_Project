59aedc867df5d70456f5ac8be875f9aa444faef6
Karl Madl <karl_madl@yahoo.com>
2021-06-24 10:25:20 -0400

    better organized the imports in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from Arduino_Reading.arduino_reading import temperature_reader
-from Weather_Data.weather_data import weather_temperature
 from Season.season_getter import get_season
 from datetime import date, datetime
+from Arduino_Reading.arduino_reading import temperature_reader
+from Weather_Data.weather_data import weather_temperature
 from MySQL.mysql_part import data_entry_to_MySQL
 
 
