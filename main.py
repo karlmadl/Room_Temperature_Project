2ad6bf0ac2f8d1f5fea69723818b619de7322449
Karl Madl <karl_madl@yahoo.com>
2021-06-27 16:36:23 -0400

    added log.txt file to log all runs as successful or unsuccessful and to hold any exceptions or error messages received if unsuccessful

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from Season.season_getter import get_season
 from Arduino_Reading.arduino_reading import temperature_reader
 from Weather_Data.weather_data import weather_temperature
 from MySQL.mysql_part import insert_into_MySQL
+from user_info.log_location import log
 
 
-data = {
-    "time": datetime.now().strftime("%H:%M"),
-    "date": date.today(),
-    "season": get_season(),
-    "inside_temperature": temperature_reader(data_points=10, seconds=10),
-    "outside_temperature": weather_temperature()
-}
+with open(log, 'a') as log:
+    log.write("\n" + "----------------------------------------------------" + "\n\n")
+    log.write(str(date.today()) + " | " + str(datetime.now().strftime("%H:%M")) + " | " + "Operation ran ")
 
-insert_into_MySQL(data)
+    try: 
+        data = {
+            "time": datetime.now().strftime("%H:%M"),
+            "date": date.today(),
+            "season": get_season(),
+            "inside_temperature": temperature_reader(data_points=10, seconds=10),
+            "outside_temperature": weather_temperature()
+        }
+
+        insert_into_MySQL(data)
+
+        log.write("successfully" + "\n")
+
+    except Exception as Argument:
+        log.write("unsuccessfully" + "\n")
+        log.write(str(Argument) + "\n")
