2b5959597b6878d4c6b06a67722dce9c89e4932c
Karl Madl <karl_madl@yahoo.com>
2021-07-16 11:11:17 -0400

    rewrote some html, added link to most recent inside temperature

--- Django/recent_temperatures/views.py
+++ Django/recent_temperatures/views.py
@@ -6,3 +6,7 @@ from .models import TemperatureData
 def index(request):
     data = TemperatureData.objects.all()
     return render(request, 'recent_temperatures/index.html', {'data': data})
+
+def data(request):
+    last_data_point = TemperatureData.objects.last()
+    return render(request, 'recent_temperatures/data.html', {'last_point': last_data_point})
