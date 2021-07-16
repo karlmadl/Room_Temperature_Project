f72b233cce827dacc661c0d3b60191ad0040f4fb
Karl Madl <karl_madl@yahoo.com>
2021-07-16 12:06:56 -0400

    reinstated data.html and a link leading to it in index.html

--- Django/recent_temperatures/urls.py
+++ Django/recent_temperatures/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recent-temperatures/', views.index)    # our-domain.com/recent-temperatures
+    path('recent-temperatures/', views.index),    # our-domain.com/recent-temperatures
+    path('recent-temperatures/data/', views.recent_last)
 ]
--- Django/recent_temperatures/views.py
+++ Django/recent_temperatures/views.py
@@ -6,3 +6,8 @@ from .models import TemperatureData
 def index(request):
     data = TemperatureData.objects.all()
     return render(request, 'recent_temperatures/index.html', {'data': data})
+
+
+def recent_last(request):
+    last_data = TemperatureData.objects.last()
+    return render(request, "recent_temperatures/data.html", {'last_point': last_data})
