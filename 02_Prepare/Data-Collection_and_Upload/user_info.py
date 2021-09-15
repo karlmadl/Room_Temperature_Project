5993f07ec51c8dcc154c1466d5239408c6266166
Karl Madl <karl_madl@yahoo.com>
2021-09-15 14:56:56 -0400

    removed user_info from .gitignore, blank skeleton

---
+++ 02_Prepare/Data-Collection_and_Upload/user_info.py
@@ -0,0 +1,24 @@
+arduino_info = {
+    "port": "",                     # arduino-computer connection port
+    "pin": "",                      # pin on arduino that reads voltage across thermistor
+    "resistor_resistence": 0,       # resistence of resistor in thermistor-resistor circuit
+    "coeff_1": 0,
+    "coeff_2": 0,
+    "coeff_3": 0,
+}
+
+My_SQL_credentials = {
+    "host": "",
+    "user": "",
+    "password": "",
+    "database": "",
+    "table": ""
+}
+
+log = r""
+
+driver_info = {
+    "driver_path": "",
+    "site": "",
+    "temp_xpath": ""
+}
