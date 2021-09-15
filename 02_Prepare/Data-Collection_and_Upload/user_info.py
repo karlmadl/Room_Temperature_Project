9a3bf8384d2c20a5efd02ccef7dbb60b5480b184
Karl Madl <karl_madl@yahoo.com>
2021-09-15 14:59:26 -0400

    fixed comments on user_info.py

--- 02_Prepare/Data-Collection_and_Upload/user_info.py
+++ 02_Prepare/Data-Collection_and_Upload/user_info.py
@@ -2,7 +2,7 @@ arduino_info = {
     "port": "",                     # arduino-computer connection port
     "pin": "",                      # pin on arduino that reads voltage across thermistor
     "resistor_resistence": 0,       # resistence of resistor in thermistor-resistor circuit
-    "coeff_1": 0,
+    "coeff_1": 0,                   # Steinhart-Hart coefficients for thermistor
     "coeff_2": 0,
     "coeff_3": 0,
 }
@@ -18,7 +18,7 @@ My_SQL_credentials = {
 log = r""
 
 driver_info = {
-    "driver_path": "",
-    "site": "",
-    "temp_xpath": ""
+    "driver_path": "",              # path to local webdriver to do the webscraping
+    "site": "",                     # site address to get outside temperature from
+    "temp_xpath": ""                # HTML xpath of element containing current temperature
 }
